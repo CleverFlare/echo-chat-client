29914748ed70c8e2c460ff62162def084bb4e65d
Muhammad Maher <blastclever@gmail.com>
2024-10-24 00:16:11 +0300

    chore: use search params for the selected chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import { ChatsList } from "@/components/chats-list";
+import { ChatsList, Connection } from "@/components/chats-list";
+
+const connections: Connection[] = [
+  {
+    id: "1",
+    name: "Muhammad Maher",
+    image:
+      "https://qph.cf2.quoracdn.net/main-qimg-5eb631ae6f587af2631f6d3348047693.webp",
+    lastMessage: {
+      date: "2024-10-09T21:00:00.000Z",
+      content: "How are you doing?",
+      state: "sent",
+    },
+  },
+  {
+    id: "2",
+    name: "Omaima Maher",
+    image:
+      "https://cdn.pixabay.com/photo/2021/06/11/12/26/woman-6328478_960_720.jpg",
+    unread: 1,
+    lastMessage: {
+      date: "2024-10-19T21:00:00.000Z",
+      content: "Bro!",
+    },
+  },
+];
 
 export default function Home() {
   return (
     <div className="h-full">
-      <ChatsList />
+      <ChatsList connections={connections} />
     </div>
   );
 }
--- src/components/chats-list.tsx
+++ src/components/chats-list.tsx
@@ -2,8 +2,9 @@
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { ChatCard } from "./chat-card";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-type Connection = {
+export type Connection = {
   id: string;
   image?: string;
   name: string;
@@ -16,8 +17,21 @@ type Connection = {
 };
 
 export function ChatsList({ connections = [] }: { connections: Connection[] }) {
-  const [active, setActive] = useState<number | string>(0);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const active = searchParams.get("active") ?? null;
+  function setActive(value: string) {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("active", value);
+
+    router.replace(`${pathname}?${params.toString()}`);
+  }
+
   const [search, setSearch] = useState<string>("");
+
   return (
     <div className="w-[300px] h-full p-4 bg-gray-50 flex flex-col gap-2">
       <h2 className="text-3xl font-bold">Chats</h2>
