254f38770457061c3dd4b3d1217115ce9323dae3
Muhammad Maher <blastclever@gmail.com>
2025-05-27 20:48:15 +0300

    chore: decrease the size of dots in the typing indicator

--- src/feature/chat/components/messages-list.tsx
+++ src/feature/chat/components/messages-list.tsx
@@ -179,9 +179,9 @@ export default function MessagesList() {
                       exit={{ y: "100%", opacity: 0, height: 0 }}
                       className="w-max p-3 rounded-xl h-max flex shadow-sm bg-white rounded-ss-none gap-1 mx-4 mb-4"
                     >
-                      <div className="size-2 rounded-full bg-neutral-200 animate-murcery" />
-                      <div className="size-2 rounded-full bg-neutral-200 animate-murcery delay-200" />
-                      <div className="size-2 rounded-full bg-neutral-200 animate-murcery delay-400" />
+                      <div className="size-1.5 rounded-full bg-neutral-200 animate-murcery" />
+                      <div className="size-1.5 rounded-full bg-neutral-200 animate-murcery delay-200" />
+                      <div className="size-1.5 rounded-full bg-neutral-200 animate-murcery delay-400" />
                     </motion.div>
                   </motion.div>
                 )}
