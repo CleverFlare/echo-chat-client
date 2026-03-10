486353ff2a88d9b8932d960e193cee2db72b1573
Muhammad Maher <blastclever@gmail.com>
2026-03-11 01:34:28 +0200

    chore(shadcn avatar): remove the EMPTY_AVATAR_IMAGE constant import

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 import { cn } from "@/lib/utils";
-import { EMPTY_AVATAR_IMAGE } from "@/constants";
 
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
