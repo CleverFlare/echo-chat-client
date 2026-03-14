ea6e9496a4e6a2d964d21c463d8cb19cb467464b
Muhammad Maher <blastclever@gmail.com>
2026-03-14 02:36:40 +0200

    chore: migrate shadcn to base ui

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -26,17 +26,21 @@ export function MainSidebar() {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
-              <a href="#">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
-                  <Command className="size-4" />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Acme Inc</span>
-                  <span className="truncate text-xs">Enterprise</span>
-                </div>
-              </a>
-            </SidebarMenuButton>
+            <SidebarMenuButton
+              size="lg"
+              className="md:h-8 md:p-0"
+              render={(props) => (
+                <a href="#" {...props}>
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
+                    <Command className="size-4" />
+                  </div>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">Acme Inc</span>
+                    <span className="truncate text-xs">Enterprise</span>
+                  </div>
+                </a>
+              )}
+            ></SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -53,15 +57,15 @@ export function MainSidebar() {
                         children: item.title,
                         hidden: false,
                       }}
-                      isActive={!!matchRoute({ to: item.to })}
+                      isActive={!!matchRoute({ to: item.to, fuzzy: true })}
                       className="px-2.5 md:px-2"
-                      asChild
-                    >
-                      <Link to={item.to}>
-                        <item.icon />
-                        <span>{item.title}</span>
-                      </Link>
-                    </SidebarMenuButton>
+                      render={(props) => (
+                        <Link to={item.to} {...props}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      )}
+                    ></SidebarMenuButton>
                   </SidebarMenuItem>
                 );
               })}
@@ -82,23 +86,26 @@ export function MainSidebar() {
                 }}
                 isActive={!!matchRoute({ to: item.to })}
                 className="px-2.5 md:px-2"
-                asChild
-              >
-                <Link to={item.to}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </Link>
-              </SidebarMenuButton>
+                render={(props) => (
+                  <Link to={item.to} {...props}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </Link>
+                )}
+              ></SidebarMenuButton>
             </SidebarMenuItem>
           );
         })}
         <Link to="/">
           <Avatar
-            className="w-full h-[unset] aspect-square rounded-lg cursor-pointer"
+            className="w-full h-[unset] aspect-square"
             role="button"
             aria-description="profile button"
           >
-            <AvatarImage src="https://api.dicebear.com/9.x/open-peeps/svg?backgroundColor=d1d4f9&seed=Aiden" />
+            <AvatarImage
+              src="https://api.dicebear.com/9.x/open-peeps/svg?backgroundColor=d1d4f9&seed=Aiden"
+              className="rounded-lg"
+            />
             <AvatarFallback>MH</AvatarFallback>
           </Avatar>
         </Link>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,27 +1,20 @@
 import * as React from "react";
+import { Input as InputPrimitive } from "@base-ui/react/input";
 
 import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  children?: React.ReactNode;
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <InputPrimitive
+      type={type}
+      data-slot="input"
+      className={cn(
+        "h-9 w-full min-w-0 rounded-xl bg-input px-2.5 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
+        className,
+      )}
+      {...props}
+    />
+  );
 }
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full bg-muted rounded-xl border-input px-4 py-2 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:border-transparent disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
-Input.displayName = "Input";
 
 export { Input };
